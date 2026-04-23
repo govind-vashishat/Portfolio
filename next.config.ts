616521d9033569157622d6f3c75111f271b5919f
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2026-04-23 17:44:41 +0530

    add remotepattern for image

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   images: {
     qualities: [75, 95],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.tina.io",
+      },
+    ],
   },
   async rewrites() {
     return [
